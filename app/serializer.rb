046a2014e932846a30c9b6aef68705fa9422e9c6
Akshay Babar <akshaybabar2408@gmail.com>
2022-09-15 21:05:20 +0530

    Implemented serializer class to make test suit working

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,41 @@
+# frozen_string_literal: true
+
+# app/serializer.rb
 class Serializer
+  class << self
+    attr_reader :serializable
+  end
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def self.attribute(field, &block)
+    @serializable ||= {}
+    @serializable[field] = block
+  end
+
+  def serialize
+    extract_attrs
+  end
+
+  private
+
+  def extract_attrs
+    resource_serializable.map { |key, proc| key_arr(key, proc) }.to_h
+  end
+
+  def key_arr(key, proc)
+    [key, attribute_value(key, proc)]
+  end
+
+  def resource_serializable
+    self.class.serializable
+  end
+
+  def attribute_value(key, proc)
+    proc.nil? ? object.public_send(key) : instance_eval(&proc)
+  end
 end
